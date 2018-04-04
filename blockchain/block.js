1268333941593f230ba69271e364fb898f8faded
Paul Joe George(JP_SMN) <31526130+paul-ml@users.noreply.github.com>
2018-04-04 13:01:36 +0900

    refs #dynamic difficulty for block is added WITHOUT TEST CODE

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,52 +1,63 @@
 const SHA256 = require('crypto-js/sha256');
 
-const { DIFFICULTY } = require('../config');
+const { DIFFICULTY, MINE_RATE } = require('../config');
 
 class Block {
-	constructor(timestamp, lastHash, hash, data, nonce) {
-		this.timestamp = timestamp;
-		this.lastHash  = lastHash;
-		this.hash      = hash;
-		this.nonce     = nonce;
-		this.data      = data;
+	constructor(timestamp, lastHash, hash, data, nonce, difficulty) {
+		this.timestamp  = timestamp;
+		this.lastHash   = lastHash;
+		this.hash       = hash;
+		this.nonce      = nonce;
+		this.difficulty = difficulty || DIFFICULTY;
+		this.data       = data;
 	}
 
 	toString() {
 		return `Block - 
-			Timestamp: ${this.timestamp}
-			LastHash : ${this.lastHash.substring(0,10)}
-			Hash     : ${this.hash.substring(0,10)}
-			Nonce    : ${this.nonce}
-			Data     : ${this.data}`
+			Timestamp  : ${this.timestamp}
+			LastHash   : ${this.lastHash.substring(0,10)}
+			Hash       : ${this.hash.substring(0,10)}
+			Nonce      : ${this.nonce}
+			Difficulty : ${this.difficulty}
+			Data       : ${this.data}`
 	}
 // Keyword static helps to call genesis method directly (Block.genesis())
 	static genesis() {
-		return new this('genesis time stamp', '-----', 'f156-h412', [], 0);	// this will call the same class Block 
+		return new this('genesis time stamp', '-----', 'f156-h412', [], 0, DIFFICULTY);	// this will call the same class Block 
 	}
 
 	static mineBlock(lastBlock, data) {
 		let   timestamp, hash
 		const lastHash    = lastBlock.hash;
 		let   nonce       = 0;
+		let {difficulty}  = lastBlock;
 		
 		do {
 			nonce++;
-			timestamp = Date.now();
-			hash      = Block.hash(timestamp, lastHash, data, nonce);
+			timestamp  = Date.now();
+			difficulty = Block.adjustDifficulty(lastBlock, timestamp); // pass lastblock to find the timestamp of lastblock 
+			hash       = Block.hash(timestamp, lastHash, data, nonce);
 
-		}while(hash.substring(0, DIFFICULTY) !== '0'.repeat(DIFFICULTY));	//until generate hash is started by 4 0's iterate the nonce value and hash
+		}while(hash.substring(0, difficulty) !== '0'.repeat(difficulty));	//until generate hash is started by 4 0's iterate the nonce value and hash
 
-		return new this(timestamp, lastHash, hash, data, nonce);
+		return new this(timestamp, lastHash, hash, data, nonce, difficulty);
 	}
 
-	static hash(timestamp, lastBlock, data, nonce){
-		return SHA256(`${timestamp}${lastBlock}${data}${nonce}`).toString();
+	static hash(timestamp, lastBlock, data, nonce, difficulty){
+		return SHA256(`${timestamp}${lastBlock}${data}${nonce}${difficulty}`).toString();
 	}
 
 	static blockHash(block) {
-		const {timestamp, lastHash, data, nonce} = block;
+		const {timestamp, lastHash, data, nonce, difficulty} = block;
 
-		return Block.hash(timestamp, lastHash, data, nonce);
+		return Block.hash(timestamp, lastHash, data, nonce, difficulty);
+	}
+
+	static adjustDifficulty(lastBlock, currentTime) {
+		let {difficulty} = lastBlock;
+		// checks whether previous time + mine_rate > current_time   if YES  mine_rate has to be increased 
+		difficulty = lastBlock.timestamp + MINE_RATE > currentTime ? difficulty +1 : difficulty -1; 
+		return difficulty;
 	}
 }
 

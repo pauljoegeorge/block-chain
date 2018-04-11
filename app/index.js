45f3085b91f9ae32ff189268b14dc02a422d7b26
Paul Joe George(JP_SMN) <31526130+paul-ml@users.noreply.github.com>
2018-04-11 13:00:02 +0900

    there is a problem in peer to peer connection after 1 transaction

--- app/index.js
+++ app/index.js
@@ -2,11 +2,15 @@ const express    = require('express');	//for GET
 const Blockchain = require('../blockchain');
 const bodyParser = require('body-parser');  // for POST
 const P2pServer  = require('./p2p-server');
+const Wallet     = require('../wallet');  // creates a new trasactions and submit it to transaction-pool
+const TransactionPool = require('../wallet/transaction-pool')
 
-const HTTP_PORT = process.env.HTTP_PORT || 3001;			// while running we can use HTTP_PORT:3000 npm run dev
+const HTTP_PORT = process.env.HTTP_PORT || 3001; // while running we can use HTTP_PORT:3000 npm run dev
 const app       = express();
 const bc        = new Blockchain();
-const p2pserver = new P2pServer(bc); 
+const wallet    = new Wallet();
+const tp        = new TransactionPool();
+const p2pserver = new P2pServer(bc, tp); 
 
 app.use(bodyParser.json());
 
@@ -22,6 +26,20 @@ app.post('/mine', (req, res) => {
 	p2pserver.syncChains();
 
 	res.redirect('/block');
+})
+
+app.get('/transactions', (req, res) =>{  // Get all transaction (initially expecting empty array)
+	res.json(tp.transactions)
+})
+
+//Post a new transaction  (this is a transaction of single user and it is not shared among others for now)
+app.post('/transact', (req, res) =>{
+	const { recipient, amount } = req.body
+	const transaction = wallet.createTransaction(recipient, amount, tp);
+	
+	p2pserver.broadcastTransaction(transaction)
+
+	res.redirect('/transactions');
 })
 
 app.listen(HTTP_PORT, () => console.log(`listening to port ${HTTP_PORT}`));
@@ -41,3 +59,5 @@ p2pserver.listen();
 //npm run dev -> now it will create a connection with HTTP_PORT 3001 AND P2P_PORT 5001 
 
 //Now create another peer connection with http_port and p2p_port a new port numbers and set PEERS = 5001
+
+// HTTP_PORT=3002 P2P_PORT=5002 PEERS=ws://localhost:5001 npm run dev
--- app/p2p-server.js
+++ app/p2p-server.js
@@ -3,9 +3,15 @@ const WebSocket = require('ws');
 const P2P_PORT  = process.env.P2P_PORT || 5001;
 const peers	    = process.env.PEERS ? process.env.PEERS.split(',') : [];
 
+const MESSAGE_TYPES = {
+	chain: 'CHAIN',
+	transaction: 'TRANSACTION'
+}
+
 class P2pServer {
-	constructor(blokchain){
+	constructor(blokchain, transactionPool){
 		this.blockchain = blokchain;
+		this.transactionPool = transactionPool;
 		this.sockets    = [];
 	}
 
@@ -36,22 +42,52 @@ class P2pServer {
 	}
 
 	sendChain(socket){
-		socket.send(JSON.stringify(this.blockchain.chain));	//the one who created block will send the data and 
+		socket.send(JSON.stringify({
+			type: MESSAGE_TYPES.chain,
+			chain: this.blockchain.chain
+		}));	//the one who created block will send the data and 
 															//it can be received by everyone using "socket.on"
+	}
+	sendTransaction(socket, transaction){
+		socket.send(JSON.stringify({
+			type: MESSAGE_TYPES.transaction,
+			transaction
+		}));
 	}
 
 	syncChains() {
 		this.sockets.forEach(socket => this.sendChain(socket));  //will access each connected socket and replace with new valid chain
 	}
 
+	broadcastTransaction(transaction){  //broadcast each transaction to all the sockets connected (same as synChain)
+		this.sockets.forEach(socket => this.sendTransaction(socket, transaction));
+	}
+
 	messageHandler(socket) {
 		socket.on('message', message => {
 			const data = JSON.parse(message);	//all peers will receive the new block added  //this data is a chain
 			// console.log('data received from sender: ', data);
-
-			this.blockchain.replaceChain(data);
+			
+			switch(data.type){ //chain or transaction
+				case MESSAGE_TYPES.chain:
+					this.blockchain.replaceChain(data.chain);  //data.chain = this.blockchain.chain
+					break;
+				case MESSAGE_TYPES.transaction:
+					this.transactionPool.updateOrAddTransaction(data.transaction)
+					break;
+			}
+			// this.blockchain.replaceChain(data);
 		});
 	}
 } 
 
 module.exports = P2pServer;
+
+// when we are sending data through socket , message handler is expecting  a chain of data, but when we sending transaction it will try 
+		// to replace the chain the data but it will result in conflict.
+//Inorder to solve this issue 2 methods can be used.
+ //1. we can specify the type while sending the data through message handler
+ // 2. we can specify the updateHandler to update the message based on the type
+ 		// ie; specify the MESSAGE_TYPE  >> while sending chain and transaction , rather than sending block and transaction only, 
+ 			//include its type as well
+ 		// Now update the messageHandler
